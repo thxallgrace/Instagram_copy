959849c3431fdcfbfc3c9acb18ecc1f9bd7a7782
thxallgrace <thxall92@gmail.com>
2021-02-20 14:50:13 +0900

    added login activity

---
+++ app/src/main/java/com/thxallgrace/instagram_copy/LoginActivity.kt
@@ -0,0 +1,12 @@
+ package com.thxallgrace.instagram_copy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
