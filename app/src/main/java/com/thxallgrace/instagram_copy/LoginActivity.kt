b394b4a7c49f6b962f422442980fe9a69894ffac
thxallgrace <thxall92@gmail.com>
2021-02-20 16:35:38 +0900

    firebase를 통한 google 로그인 연결

--- app/src/main/java/com/thxallgrace/instagram_copy/LoginActivity.kt
+++ app/src/main/java/com/thxallgrace/instagram_copy/LoginActivity.kt
@@ -4,12 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    // Firebase Authentication 관리 클래스
     var auth: FirebaseAuth? = null
+
+    // GoogleLogin 관리 클래스
+    var googleSignInClient : GoogleSignInClient? = null
+
+    //GoogleLogin
+    val GOOGLE_LOGIN_CODE = 9001 // Intent Request ID
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -19,6 +34,32 @@ class LoginActivity : AppCompatActivity() {
 
         email_login_button.setOnClickListener{
             signinAndSignup()
+        }
+
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        //구글 로그인 클래스를 만듬
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        //구글 로그인 버튼 세팅
+        google_sign_in_button.setOnClickListener { googleLogin() }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // 구글에서 승인된 정보를 가지고 오기
+        if(requestCode == GOOGLE_LOGIN_CODE) {
+
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if(result!!.isSuccess){
+                val account = result.signInAccount
+                firebaseAuthWithGoogle(account!!)
+            }
         }
     }
 
@@ -43,6 +84,23 @@ class LoginActivity : AppCompatActivity() {
             email_edittext.text.toString(),
             password_edittext.text.toString()
         )
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    moveMainPage(task.result?.user)
+                } else {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    fun googleLogin() {
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    }
+
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth?.signInWithCredential(credential)
             ?.addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     moveMainPage(task.result?.user)
