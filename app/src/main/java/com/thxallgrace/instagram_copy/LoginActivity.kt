c273794eb6b659ea1db47fa89d4c73afed9650cc
thxallgrace <thxall92@gmail.com>
2021-02-20 17:27:17 +0900

    firebase를 통한 facebook 로그인 연동

--- app/src/main/java/com/thxallgrace/instagram_copy/LoginActivity.kt
+++ app/src/main/java/com/thxallgrace/instagram_copy/LoginActivity.kt
@@ -1,18 +1,23 @@
 package com.thxallgrace.instagram_copy
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.AccessToken
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.*
 import kotlinx.android.synthetic.main.activity_login.*
+import java.util.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -20,10 +25,13 @@ class LoginActivity : AppCompatActivity() {
     var auth: FirebaseAuth? = null
 
     // GoogleLogin 관리 클래스
-    var googleSignInClient : GoogleSignInClient? = null
+    var googleSignInClient: GoogleSignInClient? = null
 
     //GoogleLogin
     val GOOGLE_LOGIN_CODE = 9001 // Intent Request ID
+
+    // Facebook 로그인 처리 결과 관리 클래스
+    var callBackManager: CallbackManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,31 +40,34 @@ class LoginActivity : AppCompatActivity() {
         // Firebase 로그인 통합 관리하는 Object 만들기
         auth = FirebaseAuth.getInstance()
 
-        email_login_button.setOnClickListener{
+        email_login_button.setOnClickListener {
             signinAndSignup()
         }
 
-        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             .requestIdToken(getString(R.string.default_web_client_id))
             .requestEmail()
             .build()
 
         //구글 로그인 클래스를 만듬
         googleSignInClient = GoogleSignIn.getClient(this, gso)
+        callBackManager = CallbackManager.Factory.create()
 
-        //구글 로그인 버튼 세팅
         google_sign_in_button.setOnClickListener { googleLogin() }
-
+        facebook_login_button.setOnClickListener { facebookLogin() }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+        // Facebook SDK로 값 넘겨주기
+        callBackManager?.onActivityResult(requestCode, resultCode, data)
+
         // 구글에서 승인된 정보를 가지고 오기
-        if(requestCode == GOOGLE_LOGIN_CODE) {
+        if (requestCode == GOOGLE_LOGIN_CODE) {
 
             val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
-            if(result!!.isSuccess){
+            if (result!!.isSuccess) {
                 val account = result.signInAccount
                 firebaseAuthWithGoogle(account!!)
             }
@@ -110,8 +121,41 @@ class LoginActivity : AppCompatActivity() {
             }
     }
 
+    fun facebookLogin() {
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, Arrays.asList("public_profile", "email"))
+        LoginManager.getInstance()
+            .registerCallback(callBackManager, object : FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult) {
+                    handleFacebookAccessToken(result.accessToken)
+                }
+
+                override fun onCancel() {
+
+                }
+
+                override fun onError(error: FacebookException?) {
+
+                }
+
+            })
+    }
+
+    // Facebook 토큰을 Firebase로 넘겨주는 코드
+    fun handleFacebookAccessToken(token: AccessToken) {
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    moveMainPage(task.result?.user)
+                } else {
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
     fun moveMainPage(user: FirebaseUser?) {
-        if(user != null) {
+        if (user != null) {
             startActivity(Intent(this, MainActivity::class.java))
         }
     }
